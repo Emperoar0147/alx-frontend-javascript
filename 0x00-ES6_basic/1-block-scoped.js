239dbdbeddbe4894b55ea1fef54bfbdf0669e778
“Sylvanus <sylvanusuzor@gmail.com>
2024-09-30 16:03:47 +0000

    second

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  let task = false;
+  let task2 = true;
 
   if (trueOrFalse) {
-    let task = true;  // Block-scoped, doesn't overwrite outer `task`
-    let task2 = false; // Block-scoped, doesn't overwrite outer `task2`
+    let task = true;
+    let task2 = false;
   }
 
-  return [task, task2]; // Returns the values from the outer scope
+  return [task, task2];
 }
